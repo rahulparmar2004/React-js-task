f47e1056cdb67ebb38d23c3de19bcbf9c55cab49
rahulparmar2004 <rahulp09122004@gmail.com>
2026-03-02 21:48:18 +0530

    Update App.jsx

--- 08-Task/App.jsx
+++ 08-Task/App.jsx
@@ -1,19 +1,3 @@
-// import {useEffect, useState, useMemo} from 'react'
-// import { getAuth , onAuthStateChanged } from 'firebase/auth'
-// import {app} from "./Componet/Firebase"
-// import Registration from './Componet/Registration'
-// import Login from './Componet/Login'
-// import { set } from 'firebase/database'
-// import { signOut } from 'firebase/auth/cordova'
-// import ReadData from './Componet/Todo_Firebase'
-// import FireStore from './Componet/FireStore'
-// import Bg_changer from './Component/Bg_changer'
-// import Usestate_task from "./Component/Usestate_task";
-// import UseEffect_task from './Component/UseEffect_task';
-// import Useref_task from './Component/Useref_task';
-// import Form_task from './Component/Form_task';
-// import { Count } from './Redux/Count';
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./pages/Header";
 import Home from "./pages/Home";
@@ -21,47 +5,6 @@ import AddProduct from "./pages/AddProduct";
 import EditProduct from "./pages/EditProduct";
 import ViewProduct from "./pages/ViewProduct";
 
-// const auth = getAuth(app)
-
-
-// const App = () => {
-//   const [user , setUser] = useState(null)
-//   useEffect (() => {
-//   onAuthStateChanged(auth , (user) => {
-//     if(user) {
-//       setUser(user)
-//     }else {
-//       setUser(null)
-//     }
-//   })
-// },[])
-
-//   if(user !== null) {
-//     return (
-//     <div>
-//       <h1>Hello {user.email}</h1>
-//       <button onClick={() => signOut(auth)}>Logout</button>
-//     </div>
-//     )
-//   }
-
-//   return (
-//     <div>
-//       {/* <Bg_changer /> */}
-//       {/* <Usestate_task /> */}
-//       {/* <UseEffect_task /> */}
-//       {/* <Useref_task /> */}
-//       {/* <Form_task /> */}
-//       {/* <Count /> */}
-//       {/* <Registration /> */}
-//       {/* <ReadData /> */}
-//       {/* <Login /> */}
-//       <FireStore />
-//     </div>
-//   )
-// }
-
-// export default App
 
 function App() {
   return (
@@ -76,5 +19,6 @@ function App() {
     </BrowserRouter>
   );
 }
+
 
 export default App;
